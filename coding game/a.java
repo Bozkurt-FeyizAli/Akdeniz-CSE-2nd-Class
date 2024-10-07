f891765bbb6d6a5c5da60803655d7573c2e3c352
Bozkurt-FeyizAli <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-10-07 17:59:15 +0300

    07,10,2024

--- coding game/a.java
+++ coding game/a.java
@@ -10,13 +10,22 @@ class Player {
         // Keep track of used pod IDs
         Set<Integer> usedPodIds = new HashSet<>();
         int nextPodId = 42; // Start with the first pod ID at 42 (you can change this starting value)
+        int resources=0;
+        int numTravelRoutes=0;
+        List<Route> routes=new ArrayList<>();
+        Set<String> existingRoutes = new HashSet<>();
+        int numPods=0; 
+        List<Pod> pods = new ArrayList<>(); // store pod routes
+        int numNewBuildings=0;
+        List<Building> newBuildings = new ArrayList<>();
 
         // game loop
         while (true) {
-            int resources = in.nextInt(); // total resources available for the month
-            int numTravelRoutes = in.nextInt(); // number of existing routes
-            List<Route> routes = new ArrayList<>(); // store routes information
-            Set<String> existingRoutes = new HashSet<>(); // to track valid routes
+            resources = in.nextInt(); // total resources available for the month
+            numTravelRoutes = in.nextInt(); // number of existing routes
+            routes = new ArrayList<>(); // store routes information
+            existingRoutes = new HashSet<>();
+            // to track valid routes
             for (int i = 0; i < numTravelRoutes; i++) {
                 int buildingId1 = in.nextInt();
                 int buildingId2 = in.nextInt();
@@ -26,11 +35,11 @@ class Player {
                 existingRoutes.add(buildingId2 + "-" + buildingId1); // store reverse as well
             }
             
-            int numPods = in.nextInt(); // number of transport pods
+            numPods = in.nextInt(); // number of transport pods
             if (in.hasNextLine()) {
                 in.nextLine();
             }
-            List<Pod> pods = new ArrayList<>(); // store pod routes
+            pods = new ArrayList<>(); // store pod routes
             for (int i = 0; i < numPods; i++) {
                 String podProperties = in.nextLine();
                 pods.add(new Pod(podProperties)); // parse pod data
@@ -41,11 +50,11 @@ class Player {
                 usedPodIds.add(pod.id); // Track IDs of existing pods
             }
 
-            int numNewBuildings = in.nextInt(); // number of new buildings
+            numNewBuildings = in.nextInt(); // number of new buildings
             if (in.hasNextLine()) {
                 in.nextLine();
             }
-            List<Building> newBuildings = new ArrayList<>();
+            newBuildings = new ArrayList<>();
             for (int i = 0; i < numNewBuildings; i++) {
                 String buildingProperties = in.nextLine();
                 newBuildings.add(new Building(buildingProperties)); // parse building data
@@ -117,27 +126,52 @@ class Player {
     }
 
     // Class to represent a pod's properties
-    static class Pod {
-        int id;
-        String properties;
+    static class Pod extends Building {
+        int numAstronouts;
+        ArrayList<Integer> astronouts= new ArrayList<>();
 
         Pod(String properties) {
-            String[] parts = properties.split(" ");
-            this.id = Integer.parseInt(parts[0]);
-            this.properties = properties;
+            super(properties);
+            String[] s=properties.split(" ");
+            numAstronouts=Integer.parseInt(s[4]);
+            for (int i = 5; i < s.length; i++) {
+                astronouts.add(Integer.parseInt(s[i]));
+            }
         }
     }
 
     // Class to represent a building and its properties
-    static class Building {
+    static class Building  {
         int id;
-        String properties;
+        int x;
+        int y;
 
         Building(String properties) {
             String[] parts = properties.split(" ");
-            this.id = Integer.parseInt(parts[0]);
-            this.properties = properties;
+            this.id = Integer.parseInt(parts[1]);
+            x=Integer.parseInt(parts[2]);
+            y=Integer.parseInt(parts[3]);
         }
+        public double distanceToBuilding(Building a){
+            return distance(this, a);
+        }
+    }
+    static class LunarModule extends Building{
+
+        LunarModule(String properties) {
+            super(properties);
+        }
+        
+    }
+
+
+    public static double distance(Building b1, Building b2) {
+        return Math.sqrt((b1.x-b2.x)*(b1.x-b2.x) + (b2.y - b2.y)*(b2.y - b2.y));
+    }
+    
+    public static boolean pointOnSegment(Building A,Building B,Building C) {
+        double epsilon = 0.0000001;
+        return (-epsilon < (distance(B, A) + distance(A, C)) && distance(B, C) < epsilon);
     }
 }
 
@@ -275,3 +309,10 @@ class Player {
 }
 
  */
+
+
+
+
+ /*
+  * 
+  */
