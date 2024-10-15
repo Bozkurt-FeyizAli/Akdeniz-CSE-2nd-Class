429844f9d17d2ece9347458ea5704065aebd8475
Bozkurt-FeyizAli <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-10-15 23:01:10 +0300

    Door

--- Object Oriented/DogDoor2.0/DogDoorSimulator.java
+++ Object Oriented/DogDoor2.0/DogDoorSimulator.java
@@ -1,88 +1,44 @@
 import java.util.Timer; 
-import java.util.TimerTask; 
+import java.util.TimerTask;  
 
-public class DogDoorSimulator { 
+public class DogDoorSimulator{ 
     public static void main(String[] args) { 
-    DogDoor door = new DogDoor(); 
-    Remote remote = new Remote(door); 
-    // We need a new simulator!
-     System.out.println("Fido barks to go outside..."); 
-    remote.pressButton(); 
-    System.out.println("\nFido has gone outside..."); 
-    //  Our old simulator assumes Todd 
-    // and Gina are closing the door 
-    // manually, and not letting the 
-    // timer do its work. 
-    // Let’s update our simulator to 
-    // make it work with the updated 
-    // class.
-    System.out.println("\nFido’s all done..."); 
-     
     
-    System.out.println("\nFido’s back inside...");
+    } 
     }
-
-}
-
-
-class DogDoor { 
-    private boolean open; 
-    public DogDoor() { 
-    this.open = false; 
-    } 
-    public void open() { 
-    System.out.println("The dog door opens."); 
-    open = true; 
-    } 
-    public void close() { 
-    System.out.println("The dog door closes."); 
-    open = false; 
-    } 
-    public boolean isOpen() { 
-    return open; 
-    } 
-}
-
-    class Remote { 
-        private DogDoor door; 
-        public Remote(DogDoor door) { 
-        this.door = door; 
+    class DogDoor { 
+        boolean open;
+        public void open() { 
+        System.out.println("Thedog dooropens."); 
+        open= true; 
+        final Timer timer= new Timer(); 
+        timer.schedule(new TimerTask() { 
+        public void run() { 
+        close(); 
+        timer.cancel(); 
         } 
-        // isOpen
-        //  Code magnets
-        //  public void pressButton() { 
-        // System.out.println("Pressing the remote control button..."); 
-        // if (door.isOpen()) { 
-        // door.close(); 
-        // } else { 
-        // door.open(); 
-        // } 
-        // } 
-
-        public void pressButton() { 
-            System.out.println("Pressing the remote control button..."); 
-            if (door.isOpen()) { 
-            door.close(); 
-            } else { 
-            door.open(); 
-            final Timer timer = new Timer(); 
-            timer.schedule(new TimerTask() { 
-            public void run() { 
-            door.close(); 
-            timer.cancel(); 
-            } 
-            }, 5000);
-             } 
-            } 
+        }, 5000); 
+        } 
+        public void close() { 
+        System.out.println("Thedog doorcloses."); 
+        open= false; 
+        } 
         }
-        class BarkRecognizer { 
-            private DogDoor door; 
-            public BarkRecognizer(DogDoor door) { 
-            this.door=door; 
-            } 
-            public void recognize(String bark) { 
-            System.out.println(" BarkRecognizer: Heard a ‘" +
-             bark + "’"); 
-            door.open(); 
-            } 
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
