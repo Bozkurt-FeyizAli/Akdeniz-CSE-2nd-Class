5c69a7866c5ceabdf2883d820cdd115942e7fc0c
Bozkurt-FeyizAli <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-10-15 23:02:20 +0300

    main test

--- Object Oriented/DogDoor2.0/DogDoorSimulator.java
+++ Object Oriented/DogDoor2.0/DogDoorSimulator.java
@@ -3,7 +3,22 @@ import java.util.TimerTask;
 
 public class DogDoorSimulator{ 
     public static void main(String[] args) { 
-    
+        DogDoor door =new DogDoor(); 
+        BarkRecognizer recognizer=new BarkRecognizer(door); 
+        Remote remote =new Remote(door); 
+        // Simulatethehardware hearing a bark
+         System.out.println("Fidostartsbarking."); 
+        recognizer.recognize("Woof"); 
+        System.out.println("\nFido has goneoutside..."); 
+        System.out.println("\nFido’salldone..."); 
+        try{ 
+        Thread.currentThread().sleep(10000); 
+        } catch(InterruptedException e) { } 
+        System.out.println("...but he’sstuck outside!"); 
+        // Simulatethehardware hearing a bark again
+         System.out.println("Fidostartsbarking."); 
+        recognizer.recognize("Woof"); 
+        System.out.println("\nFido’sback inside..."); 
     } 
     }
     class DogDoor { 
@@ -67,7 +82,9 @@ public class DogDoorSimulator{
                      "Heard a ‘" + bark + "’"); 
                     door.open(); 
                     }
-                }    
+                }
+                
+                
 
 
 
