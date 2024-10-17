360cb24223316dd6e63cec170a8b055072103687
Bozkurt-FeyizAli <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-10-17 12:53:28 +0300

    peek

--- Data Structures Lab/for lesson/4th week/piotityQueue.java
+++ Data Structures Lab/for lesson/4th week/piotityQueue.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class piotityQueue {
     
 }
@@ -49,11 +51,61 @@ interface IPriorityQueue<T> {
     int size();
 }
 
+class PiorityQueue<T> implements IPriorityQueue<T>{
+    private int size;
+    private PriorityQueueNode<T>[] datas;
+    private static final int capacity=15;
 
+    public PiorityQueue(int capacity){
+        size=0;
+        datas= new PriorityQueueNode[capacity];
+    }
+
+    public PiorityQueue(){
+        this(capacity);
+    }
+    @Override
+    public void enqueue(T item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'enqueue'");
+    }
+
+    @Override
+    public T dequeue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty() || datas == null)
+            throw new NoSuchElementException();
+        int priority=0;
+        T priotiryNode=null;
+        for (int i = 0; i < size; i++) {
+            if(datas[i].getPriority()>priority){
+                priotiryNode=datas[i].getData();
+                priority=datas[i].getPriority();
+            }
+        }
+        return priotiryNode;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public int size() {
+       return size;
+    }
+    
+}
 
 class PriorityQueueNode<T> implements IPriorityQueueNode<T>{
-    T data;
-    int priority;
+    private T data;
+    private int priority;
 
     public PriorityQueueNode(T data, int priority){
         this.data=data;
