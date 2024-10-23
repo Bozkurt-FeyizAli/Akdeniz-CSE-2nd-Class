07902ab4670873617f1ca7dee3898e5f2fbbc684
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-10-23 13:35:10 +0300

    rotateRight

--- Letcode/week2.java
+++ Letcode/week2.java
@@ -1,6 +1,14 @@
 public class week2 {
     public static void main(String[] args) {
         
+        ListNode ln1= new ListNode(5);
+        ListNode ln2= new ListNode(4, ln1);
+        ListNode ln3= new ListNode(3, ln2);
+        ListNode ln4= new ListNode(2, ln3);
+        ListNode ln5= new ListNode(1, ln4);
+        rotateRight(ln5, 2);
+
+
 
 
     }
@@ -10,4 +18,33 @@ public class week2 {
         String[] words=s.split(" ");
         return words[words.length-1].length();
     }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        
+        for (int i = 0; i < k; i++) {
+            rotateRight(head);
+        }
+        return head;
+    }
+
+    public static void rotateRight(ListNode head) {  // not finished
+        ListNode h=head;
+        int current=0;
+        while (h.next!=null) {
+            
+            current=h.next.val;
+            h.next.val=h.val;
+            h=h.next;
+
+        }
+        head.val=h.val;
+    }
+}
+
+class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
