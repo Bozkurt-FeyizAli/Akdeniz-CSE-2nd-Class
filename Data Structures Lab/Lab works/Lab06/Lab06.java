6ac591843e9fe8a92d2e15841301227912fa5b22
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-10-30 19:40:25 +0300

    Update Lab06.java

--- Data Structures Lab/Lab works/Lab06/Lab06.java
+++ Data Structures Lab/Lab works/Lab06/Lab06.java
@@ -4,8 +4,12 @@ import java.util.List;
 public class Lab06 {
     public static void main(String[] args) {
         
+    
+
+
     }
 }
+
 
 // TreeNode<E> generic class represents a single node in the tree
 /*
@@ -32,12 +36,18 @@ public class Lab06 {
 
   class TreeNode<E>{
     private E data;
-    private ArrayList<TreeNode<E>> listOfChildren;
+    private List<TreeNode<E>> listOfChildren;
     private TreeNode<E> parent;
 
     public TreeNode(E data, TreeNode<E> parent){
         this.data=data;
         this.parent=parent;
+        listOfChildren= new ArrayList<>();
+    }
+
+    public TreeNode(E data){
+        this.data=data;
+        this.parent=null;
         listOfChildren= new ArrayList<>();
     }
 
@@ -48,7 +58,7 @@ public class Lab06 {
     public E getData() {
         return data;
     }
-    public ArrayList<TreeNode<E>> getChildren() {
+    public List<TreeNode<E>> getChildren() {
         return listOfChildren;
     }
     public TreeNode<E> getParent() {
@@ -62,35 +72,52 @@ public class Lab06 {
     public GeneralTree(){
         root=null;
     }
+    public GeneralTree(TreeNode<E> node){
+        root=node;
+    }
 
     public void addRootChild(E data){
         if(root!=null)
             root.addChild(new TreeNode<>(data, root));
+        else
+            root=new TreeNode<>(data, root); 
     }
 
     public int getHeight(TreeNode<E> node){
-        if(root==null||root.getChildren()==null)
-            return 0;
-        int height=0;
-        for (TreeNode<E> child : root.getChildren()) {
-            int h=0;
-            for (TreeNode<E> c : child.getChildren()) {
-                h++;
-            }
-            if(h>height)
-                height=h;
-        }
-        return height+1;
+        
+        int h = 0;
+        if(node.getChildren()==null)
+        for (var a : node.getChildren())
+            h = Math.max(h, getDepth(a));
+        return h;
     }
 
     public int getDepth(TreeNode<E> node){
-        if(root==null||root.getParent()==null)
+        if(node==root)
             return 0;
-        int result=0;
-        while (node.getParent()!=null) {
-            result++;
-            node=node.getParent();
-        }
-        return result;
+        return 1+ getDepth(node.getParent());
     }
-  }
+
+    public void preorder(TreeNode<E> node){
+        if(node.getParent()==null||node==null)
+        return;
+        preorder(node.getParent());
+        System.out.println(node.getData());
+    }
+
+    public void postorder(TreeNode<E> node){
+        if(node==null)
+            return;
+        if(node.getChildren()==null)
+            return;
+        
+        for (var p : node.getChildren()) {
+            System.out.println(p.getData());
+            postorder(p);
+        }
+    }
+
+    public TreeNode<E> getRoot() {
+        return root;
+    }
+}
