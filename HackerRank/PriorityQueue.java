7cb459da6d3e3dceb480d92b5430d4f1adac5bfc
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-02 22:05:09 +0300

    Update PriorityQueue.java

--- HackerRank/PriorityQueue.java
+++ HackerRank/PriorityQueue.java
@@ -5,7 +5,42 @@ public class PriorityQueue {
 }
 
 
+interface PriorityQueueNodeInterface<E> {
+    E getElement(); // Düğümdeki elemanı döner.
+    void setElement(E element); // Düğümdeki elemanı ayarlar.
+    int getPriority(); // Düğümdeki önceliği döner.
+    void setPriority(int priority); // Düğümdeki önceliği ayarlar.
+}
 
+class PriorityQueueNode<E> implements PriorityQueueNodeInterface<E>{
+    private E element;
+    private int priority;
+    public PriorityQueueNode(E e, int priority){
+        element=e;
+        this.priority=priority;
+    }
+
+    @Override
+    public E getElement() {
+        return element;
+    }
+
+    @Override
+    public void setElement(E element) {
+        this.element=element;
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public void setPriority(int priority) {
+        this.priority=priority;
+    }
+    
+}
 
 interface PriorityQueueInterface<E> {
     void insert(E element, int priority); // Belirtilen öncelik ile bir eleman ekler.
