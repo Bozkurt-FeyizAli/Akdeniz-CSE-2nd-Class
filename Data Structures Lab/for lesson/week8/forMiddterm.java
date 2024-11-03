c815129498734ac8c91903071d6d79047166f79a
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-03 18:22:46 +0300

    Update forMiddterm.java

--- Data Structures Lab/for lesson/week8/forMiddterm.java
+++ Data Structures Lab/for lesson/week8/forMiddterm.java
@@ -1,7 +1,9 @@
 public class forMiddterm{
     public static void main(String[] args) {
         System.out.println(5/2);
-        System.out.println(reverse(-12, 0));
+        System.out.println(reverse(100000, 0));
+        System.out.println(betterNumReversse(100000));
+        betterNumReversse1(1000000);
     }
 
     public static String reverse(int number, int i){
@@ -13,5 +15,19 @@ public class forMiddterm{
         else if(num.length()/2==i)
             return "";
         else return Character.toString(num.charAt(num.length()-1-i))+ reverse(number, i+1)  + Character.toString(num.charAt(i));   
+    }
+
+    public static String betterNumReversse(int number){
+        if(number<10)
+            return Integer.toString(number);
+        return Integer.toString(number%10)+betterNumReversse(number/10);
+    }
+    public static void betterNumReversse1(int number){
+        if(number<10)
+            System.out.println( Integer.toString(number));
+        else{
+        System.out.println(Integer.toString(number%10)) ;
+        betterNumReversse1(number/10);
+        }
     }
 }
