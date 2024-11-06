41cbda89b5d0491614b45eac19d4007ee58bd7e5
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-06 19:09:45 +0300

    Update week9.java

--- Data Structures Lab/for lesson/week9/week9.java
+++ Data Structures Lab/for lesson/week9/week9.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
 import java.util.Comparator;
 public class week9{
 
 }
 
-interface IPriorityQueue{
-
+interface IPriorityQueue<T>{
+    public void enqueue(T element, int priority);
+    public T dequeue();
+    public T peek();
+    public boolean isEmpty();
+    public long size();
+    public void clear();
 }
 
+
  class PriorityQueueImpl<T> implements IPriorityQueue<T> {
-    private static class Element<T> {
+    private static class Element<T> implements Comparable<Element<T>>{
         T value;
         int priority;
         long insertionOrder;
@@ -20,44 +27,61 @@ interface IPriorityQueue{
         }
     }
 
-    private final PriorityQueueImpl<Element<T>> queue;
+    private  ArrayList<Element<T>> list;
     private long orderCounter = 0;
+    
 
     public PriorityQueueImpl() {
-        this.queue = new PriorityQueue<>(Comparator
-                .comparingInt((Element<T> e) -> -e.priority)
-                .thenComparingLong(e -> e.insertionOrder));
+        this.list=new ArrayList<>();
     }
 
     @Override
     public void enqueue(T element, int priority) {
-        queue.add(new Element<>(element, priority, orderCounter++));
+        Element<T> el= new Element<>(element, priority, orderCounter);
+        if(isEmpty())
+            list.add(el);
+        else{
+            for (int i = 0; i < list.size(); i++) {
+                Element<T> eList=list.get(i);
+                if(el.compareTo(eList)>0)
+                    if(orderCounter==1)
+                        list.addFirst(eList);
+                    else    
+                        list.add(i-1, eList);
+                if(i!=0)
+                if(el.compareTo(list.get(i+1))>0)
+                    list.add(i, el);
+            }
+            if(!list.contains(el))
+                list.add(el);
+        }
+        orderCounter++;
     }
 
     @Override
     public T dequeue() {
-        Element<T> highestPriorityElement = queue.poll();
-        return highestPriorityElement != null ? highestPriorityElement.value : null;
+        T first=list.getFirst().value;
+        list.removeFirst();
+        return first;
     }
 
     @Override
     public T peek() {
-        Element<T> highestPriorityElement = queue.peek();
-        return highestPriorityElement != null ? highestPriorityElement.value : null;
+        return list.getFirst().value;
     }
 
     @Override
     public boolean isEmpty() {
-        return queue.isEmpty();
+        return orderCounter==0;
     }
 
     @Override
-    public int size() {
-        return queue.size();
+    public long size() {
+        return orderCounter;
     }
 
     @Override
     public void clear() {
-        queue.clear();
+        list= new ArrayList<>();
     }
 }
