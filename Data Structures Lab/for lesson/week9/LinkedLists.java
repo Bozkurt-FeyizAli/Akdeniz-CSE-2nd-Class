e9e35aadaa45f26f874a20fbb0e97c0dacc0bc53
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-08 22:12:21 +0300

    Create LinkedLists.java

---
+++ Data Structures Lab/for lesson/week9/LinkedLists.java
@@ -0,0 +1,37 @@
+public class LinkedLists {
+    public static void main(String[] args) {
+        
+    }
+}
+
+interface List <E> {
+    int size(); // Returns the size of the list
+    boolean isEmpty(); // Checks whether the list is empty or not
+    E first(); // returns the element/data of first entry in the list
+    E last(); // returns te element/data of last entry in the list
+    void addFirst(E e); // adds the given element at the start of the list
+    void addLast(E e); // adds the given element at the end of the list
+    E removeFirst(); // removes the first element in the list
+}
+
+interface Circlular {
+    void rotate(); // tail becomes next of tail if not empty
+}
+
+interface Doubly <E> extends List <E> {
+    E removeLast(); //removes the last element from the list
+}
+
+interface INode <E> { // storage unit
+    E getElement(); // data/element
+    Node<E> getPrev(); // returns the previous unit of this unit
+    void setPrev(Node<E> prev); // sets the previous of this unit
+    Node<E> getNext(); // returns the next unit of this unit
+    void setNext(Node<E> next); // sets the next of this unit
+}
+
+class DoublyLinkedList<E> implements Doubly<E>{}
+
+class CircularLinkedList<E> implements List<E>, Circlular{}
+
+class SinglyLinkedList<E> implements List<E>{}
