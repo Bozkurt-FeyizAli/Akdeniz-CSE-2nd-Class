a52b70410a6f26c5b1242b56b579767bbd5134f7
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-08 22:23:48 +0300

    Update LinkedLists.java

--- Data Structures Lab/for lesson/week9/LinkedLists.java
+++ Data Structures Lab/for lesson/week9/LinkedLists.java
@@ -30,8 +30,123 @@ interface INode <E> { // storage unit
     void setNext(Node<E> next); // sets the next of this unit
 }
 
+class Node<E> implements INode<E>{
+    E data;
+    Node<E> prev;
+    Node<E> next;
+    
+    public Node(E data, Node<E> next){
+        this(data, next, null);
+    }
+    public Node(E data, Node<E> next, Node<E> prev){
+        this.data=data;
+        this.next=next;
+        this.prev=prev;
+    }
+
+    @Override
+    public E getElement() {
+        return data;
+    }
+
+    @Override
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    @Override
+    public void setPrev(Node<E> prev) {
+        this.prev=prev;
+    }
+
+    @Override
+    public Node<E> getNext() {
+        return next;
+    }
+
+    @Override
+    public void setNext(Node<E> next) {
+       this.next=next;
+    }
+    
+}
+
 class DoublyLinkedList<E> implements Doubly<E>{}
 
 class CircularLinkedList<E> implements List<E>, Circlular{}
 
-class SinglyLinkedList<E> implements List<E>{}
+class SinglyLinkedList<E> implements List<E>{
+    int size;
+    Node<E> head;
+    Node<E> tail;
+    public SinglyLinkedList(){
+        tail=null;
+        size=0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public E first() {
+       if(head==null)
+        return null;
+        return head.getElement();
+    }
+
+    @Override
+    public E last() {
+        if(tail==null)
+            return null;
+        return tail.getElement();
+    }
+
+    @Override
+    public void addFirst(E e) {
+        Node<E> newnode= new Node<>(e, null); 
+        if(isEmpty()){
+            head=tail=newnode;
+        }
+        else{
+            newnode.setNext(head);
+            head=newnode;
+        }
+        size++;
+    }
+
+    @Override
+    public void addLast(E e) {
+        Node<E> newnode= new Node<>(e, null);
+        if(isEmpty()){
+            addFirst(e);
+        }
+        else{
+            tail.setNext(newnode);
+            tail=tail.getNext();
+            size++;
+        }
+    }
+
+    @Override
+    public E removeFirst() {
+        if(isEmpty())
+            return null;
+        else{
+            E remove=head.getElement();
+            head=head.getNext();
+            size--;
+            if(size==1){
+                tail=head;
+            }
+            return remove;
+        }
+    }
+    
+}
