b1b501fa349dccad50d2dbca6d3e65cd5d9a46ce
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-22 18:04:47 +0300

    Update priorityQueue.java

--- Data Structures Lab/for lesson/week12/priorityQueue.java
+++ Data Structures Lab/for lesson/week12/priorityQueue.java
@@ -117,3 +117,163 @@ class LinkedList<T> implements ILinkedList<T>{
     }
 
     @Override
+    public T remove(int index) {
+        if(isEmpty())
+            return null;
+        else if(index>size)
+            throw new InvalidParameterException();
+        else{
+            Node<T> h=head;
+            index--;
+            while(index!=0){
+                h=h.getNext();
+                index--;
+            }
+            T data=h.getData();
+            h.getPrev().setNext(h.getNext());
+            h.getNext().setPrev(h.getPrev());
+            size--;
+            return data;
+        }
+    }
+
+    public T remove() {
+        if(isEmpty())
+            return null;
+        else{
+            T data=tail.getData();
+            if(size>1){
+            tail=tail.getPrev();
+            }
+            else{
+                tail=null;
+                head=null;
+            }
+            size--;
+            return data;
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if(isEmpty())
+            return null;
+        else if(index>size)
+            throw new InvalidParameterException();
+        else{
+            Node<T> h=head;
+            index--;
+            while(index!=0){
+                h=h.getNext();
+                index--;
+            }
+            return h.getData();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        if(head==null)
+            return "empty linkedlist";
+        return head.toString();
+    }
+    
+}
+
+ interface IPriorityQueue<T> extends IPriorityQueueItem<T> {
+    void enqueue(T item, int priority); // Kuyruğa bir öğe ekle
+    T dequeue(); // En yüksek önceliğe sahip öğeyi çıkar
+    T peek(); // En yüksek önceliğe sahip öğeyi görüntüle ama çıkarmadan
+    boolean isEmpty(); // Kuyruk boş mu?
+    int size(); // Kuyruktaki eleman sayısı
+}
+
+class PriorityQueue<T> implements IPriorityQueue<T>{
+    private final int maxSize=1000;
+    private Node<T> head;
+    private int size;
+
+    public PriorityQueue(){
+        head=null;
+        size=0;
+    }
+
+
+    @Override
+    public void enqueue(T item, int priority) {
+        Node<T> newNode= new Node<>(item, priority, null, null);
+        if(isEmpty())
+            head=newNode;
+        else{
+            if(size<maxSize){
+            Node<T> h=head;
+            while(h!=null&&newNode.compareTo(h)<=0){
+                h=h.getNext();
+            }
+            if(h!=head){
+                h.getPrev().setNext(newNode);
+                newNode.setPrev(h.getPrev());
+                newNode.setNext(h);
+                h.setPrev(newNode);
+            }
+            else{
+                newNode.setNext(h);
+                h.setPrev(newNode);
+                head=newNode;
+            }
+            }
+            else{
+                throw new InvalidParameterException();
+            }
+        }
+        size++;
+    }
+
+    @Override
+    public T dequeue() {
+        if(isEmpty())
+            return null;
+        else{
+            T data=head.getData();
+            head=head.getNext();
+            size--;
+            return data;
+        }
+    }
+
+    @Override
+    public T peek() {
+        if(isEmpty())
+            return null;
+        else{
+            return head.getData();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return head.toString();
+    }
+
+}
+
