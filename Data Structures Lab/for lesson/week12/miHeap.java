9fa88eb33488dd199b274c6a01a671776ebe1387
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-27 18:34:01 +0300

    Update miHeap.java

--- Data Structures Lab/for lesson/week12/miHeap.java
+++ Data Structures Lab/for lesson/week12/miHeap.java
@@ -69,3 +69,66 @@ interface Heap<T extends Comparable<? super T>, U> {
     int size();             // Heap'teki eleman sayısını döndürür
 }
 
+
+class minimumHeap<T extends Comparable<? super T>, U> implements Heap<T, U>{
+
+    private int size;
+    private Entry<T, U>[] array;
+
+    public minimumHeap(int c){
+        size=0;
+        array= (Entry[])new Object[c];
+    }
+    public minimumHeap(){
+        size=0;
+        array= (Entry[])new Object[10];
+    }
+    @Override
+    public void insert(T t, U u) {
+        resize();
+        array[size++]=new Entry<>(t, u);
+        recHUp(size-1);
+    }
+
+    public void resize(){
+        if(size==array.length){
+            array= Arrays.copyOf(array, size+size/2);
+        }
+    }
+
+    public void recHUp(int i) {
+        while(hasParent(i)){
+            if(array[0].getKey().compareTo(array[i].getKey())<0)
+            break;
+            swap(i, indexParent(i));
+        }
+    }
+    
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+    @Override
+    public Entry<T, U> remove() {
+        Entry<T, U> rem=array[0];
+        array[0]=array[--size];
+        array[size-1]=null;
+        recHDown(0);
+        return rem;
+    }
+    public void recHDown(int i){
+        if(indexLeft(i)!=1){
+            if(array[i].getKey().compareTo(array[indexLeft(i)].getKey())>0)
+                swap(i, indexLeft(i));
+        }
+        if(indexRight(i)!=0)
+            if(array[i].getKey().compareTo(array[indexRight(i)].getKey())>0)
+                swap(i, indexRight(i));
+    }
+    @Override
