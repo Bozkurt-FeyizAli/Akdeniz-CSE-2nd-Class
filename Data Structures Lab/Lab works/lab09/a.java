1458c11f4de62e845d331afd389131f2b9ed8fbd
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-28 22:23:24 +0300

    Create a.java

---
+++ Data Structures Lab/Lab works/lab09/a.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class a {
+    public static void main(String[] args) {
+        //NodeHeap<Integer, String> heap = new NodeHeap<>();
+        ArrayHeap<Integer, String> heap = new ArrayHeap<>(500);
+
+        // Inserting elements into the heap
+        heap.insert(10, "Ten");
+        heap.insert(20, "Twenty");
+        heap.insert(5, "Five");
+        heap.insert(30, "Thirty");
+        heap.insert(15, "Fifteen");
+        heap.insert(25, "Twenty-Five");
+        heap.insert(1, "One");
+
+        // Display the heap (level-order)
+        System.out.println("Heap after insertions (level-order):");
+        heap.levelorder();
+
+        //NodeHeap<Integer, String> heap2 = new NodeHeap<>();
+        ArrayHeap<Integer, String> heap2 = new ArrayHeap<>(500);
+
+        heap2.insert(8, "Ten");
+        heap2.insert(23, "Twenty");
+        heap2.insert(3, "Five");
+        heap2.insert(27, "Thirty");
+        heap2.insert(16, "Fifteen");
+        heap2.insert(22, "Twenty-Five");
+        heap2.insert(2, "One");
+
+                // Display the heap (level-order)
+        System.out.println("\nHeap after insertions (level-order):");
+        heap2.levelorder();
+
+        System.out.println("\nHeap after merge (level-order):");
+        //var heap3 = NodeHeap.merge(heap, heap2, 6, "Six");
+        var heap3 = ArrayHeap.merge(heap, heap2);
+        heap3.levelorder();
+        //System.out.println(heap3.getRoot().entry.getValue());
+
+        /* int[] array = {1, 5, 3, 2, 6, 4};
+        heapSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        } */
+    }
+
