92156fca720c6de24e2ca920314a78274f48ea28
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2024-11-28 22:23:14 +0300

    Update miHeap.java

--- Data Structures Lab/for lesson/week13/miHeap.java
+++ Data Structures Lab/for lesson/week13/miHeap.java
@@ -113,3 +113,70 @@ class minimumHeap<T extends Comparable<? super T>, U> implements Heap<T, U>{
     public boolean isEmpty() {
         return size==0;
     }
+
+    @Override
+    public int size() {
+        return size;
+    }
+    @Override
+    public Entry<T, U> remove() {
+        Entry<T, U> rem=array[0];
+        array[0]=array[--size];
+        array[size]=null;
+        recHDown(0);
+        return rem;
+    }
+    public void recHDown(int i){
+        while(hasLeft(i)){
+            int leftIndex=indexLeft(i);
+            int smallC=leftIndex;
+            if(hasRight(i)){
+                int rightIndex=indexRight(i);
+                if(array[leftIndex].getKey().compareTo(array[rightIndex].getKey())>0)
+                    smallC=rightIndex;
+            }
+            if(array[smallC].getKey().compareTo(array[i].getKey())>0)
+                break;
+            swap(i, smallC);
+            i=smallC;
+        }
+    }
+    @Override
+    public Entry<T, U> peek() {
+        return array[0];
+    }
+
+    public void levelorder() {
+        for (Entry<T,U> entry : array) {
+            if(entry==null)
+                System.out.println("null");
+            else
+            System.out.println( entry.getValue().toString());
+        }
+    }
+
+    public void swap(int i, int j){
+        if(i<size&&i>=0&&j<size&&j>=0){
+        Entry<T, U> swap=array[i];
+        array[i]=array[j];
+        array[j]=swap;
+        }
+        else{
+            System.out.println(i+", "+j);
+            System.out.println( "problem in swap method");
+        }
+    }
+
+    public int indexRight(int i){ return (2*i+2<size)? 2*i+2: -1;}
+    public int indexLeft(int i){ return (2*i+2<size)? 2*i+1: -1;}
+    public int indexParent(int i){
+        if(i==0)
+            return -1;
+        else
+        return ((i-1)/2<size&&(i-1)/2>-1)? (i-1)/2: -1;}
+    public boolean hasRight(int i){return indexRight(i)>0&&indexRight(i)<size;}
+    public boolean hasLeft(int i){return indexLeft(i)>0&&indexLeft(i)<size;}
+    public boolean hasParent(int i){return indexParent(i)>=0&&indexParent(i)<size;}
+    
+} 
+
