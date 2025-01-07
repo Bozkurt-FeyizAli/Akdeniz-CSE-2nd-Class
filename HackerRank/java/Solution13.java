32b533144a91f1c72c7d05dee9bfdfffdbd72447
Feyiz Ali BOZKURT <153587792+Bozkurt-FeyizAli@users.noreply.github.com>
2025-01-07 16:49:52 +0300

    07,01,24

---
+++ HackerRank/java/Solution13.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution13 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s= new Scanner(System.in);
+        String a=s.nextLine();
+        String b=s.nextLine();
+        System.out.println(a.length()+b.length());
+        
+        System.out.println((a.compareTo(b)>0)? "Yes": "No" );
+        System.out.println(Character.toUpperCase(a.charAt(0))+a.substring(1)+Character.toUpperCase(b.charAt(0))+b.substring(1));
+    }
+}
